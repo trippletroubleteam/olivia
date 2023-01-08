8a6865da10a0c454c1e6947727f3268e896bd03f
TanujKS <tanujsiripurapu@gmail.com>
2023-01-07 23:57:50 -0800

    Better parental suggestions

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ openai.api_key = os.getenv("OPENAI_KEY")
 app = Flask(__name__)
 model = model4.load_model()
 
-congrarts_prompt = "In one concise sentence, congratulate a parent on how well they are raising their child."
+congrats_prompt = "In one concise sentence, congratulate a parent on how well they are raising their child."
 help_prompt = "You are a smart family counselor tasked with advising a parent to check up on their child who is not doing too well in one concise sentence."
 danger_prompt = "You are tasked with alerting and explaining to a parent that their child is in danger of hurting themselves and must seek help in one concise sentence."
-
+counselor_prompt = "You are a smart family counselor and a child has just said the following to you, in one concise sentence tell the parents how they should react based on this information. "
 
 @app.route("/olivia")
 def olivia():
@@ -24,11 +24,10 @@ def olivia():
     depression_score = model4.test(model, text)
 
     return {
-
         "olivia": generate_completion(therapy_prompt(text)),
         "depression_score": depression_score/200,
         "sentence": text,
-        "suggestion": generate_suggestion(depression_score)
+        "suggestion": generate_completion(counselor_prompt + text)
         }
 
 
@@ -54,11 +53,11 @@ def clean_completion(text):
 
 def generate_suggestion(percent):
     if percent == 0:
-        response = generate_completion(congrarts_prompt)
+        response = generate_completion(therpay_prompt)
     elif percent > 0 and percent < 100:
-        response = generate_completion(help_prompt)
+        response = generate_completion(therpay_prompt)
     elif percent > 100:
-        response = generate_completion(danger_prompt)
+        response = generate_completion(therpay_prompt)
 
     return clean_completion(response)
 
